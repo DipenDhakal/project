3378e7bba94baa4fbb750016914cae460e0f5170
DipenDhakal <dipdhakal.dd@gmail.com>
2022-10-21 12:30:02 +0545

    hello

---
+++ script.js
@@ -0,0 +1,7 @@
+function changeColor() {
+			document.getElementById('myParagraph').style.color="red";
+
+		}
+		function defaultColor() {
+			document.getElementById('myParagraph').style.color="black";
+}
